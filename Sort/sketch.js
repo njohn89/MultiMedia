05ef9abd4801508819997c751b7d81b40eaba3c9
njohn89 <njohn89@lsu.edu>
2022-02-11 02:06:06 -0600

    purple rain

--- Sort/sketch.js
+++ Sort/sketch.js
@@ -5,18 +5,45 @@ function setup() {
   winH = $(window).height();
   winW = $(window).width();
   createCanvas(winW-100, winH-100);
-  strokeWeight(2);
-  background(255);
+  strokeWeight(1);
+  //background(255);
   
-  console.log(winH);
-  paint_color = color(0,0,0);
+  //console.log(winH);
+  //paint_color = color(0,0,0);
 }
-
-
-
 function draw() {
   //createCanvas(winW-100, winH-100);
+  dropOne = new Drop(10, 10);
+
+  //for(i = 0; i<10; i++) {
+  //  dropOne.fall();
+  //}
   
-  rect(0,winH-100, 250, 100);
-  fill("red");
+  //stroke("red");
+  //strokeWeight(0.0125);
+  //line(100, 100, 20, 20);
 }
+
+
+function Drop(xPos, yPos){
+  this.x = xPos;
+  this.y = yPos;
+  this.size = 10;
+  stroke("red");
+  this.thisLine = line(this.x, this.y, this.x+10, this.y+10);
+
+  this.fall = function() {
+    //console.log("&&");
+    
+    
+    while(this.x < winH) {
+      console.log("&&");
+      line(this.x, this.y, this.x+10, this.y+10);
+
+    }
+
+  }
+
+}
+
+
